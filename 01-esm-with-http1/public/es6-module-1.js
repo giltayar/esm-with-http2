4ced64496d115bcf82753c42c13beb35525f4e2a
Gil Tayar <gil@tayar.org>
2017-03-18 12:43:01 +0200

    es6 modules vs webpack in http/1

--- 01-esm-with-http1/public/es6-module-1.js
+++ 01-esm-with-http1/public/es6-module-1.js
@@ -1,3 +1,4 @@
-import more from './es6-module-2.js'
+import perfectly from './es6-module-2.js'
+import _ from './node_modules/lodash-es/lodash.default.js'
 
-export default () => `It works ${more}!`
+export default () => `It works ${_.toUpper(perfectly)}!`
--- 01-esm-with-http1/server.js
+++ 01-esm-with-http1/server.js
@@ -5,6 +5,6 @@ const app = express()
 
 app.set('etag', false)
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public'), {maxAge: 1000 * 1000 * 1000, etag: false}))
 
 app.listen(process.env.PORT || 3000, () => console.log('Listening...'))
