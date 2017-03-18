407d91f86ffd8493a0adadcc50e8f1f2be6a1758
Gil Tayar <gil@tayar.org>
2017-03-18 11:41:49 +0200

    es6 module starting to work

---
+++ 01-esm-with-http1/public/es6-module-1.js
@@ -0,0 +1,3 @@
+import more from './es6-module-2.js'
+
+export default () => `It works ${more}!`
--- 01-esm-with-http1/server.js
+++ 01-esm-with-http1/server.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', express.static(path.join(__dirname, 'public'), {extensions: ['html']}))
+app.set('etag', false)
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 3000, () => console.log('Listening...'))
